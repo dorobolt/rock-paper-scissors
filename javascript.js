e678053dc7b2af55746483e76a7c1920b91d5089
dorobolt <91879792+dorobolt@users.noreply.github.com>
2023-12-02 10:38:49 +0700

    Remove unnesessary line

--- javascript.js
+++ javascript.js
@@ -1,6 +1,4 @@
 // make variable
-let computer;
-let player;
 let computerChoice;
 let playerChoice;
 // Scoreboard
@@ -10,10 +8,10 @@ let cscore = 0;
 //make function for player to choose
 function getPlayerChoice() {
     // summon prompt to input choice
-    player = prompt("Choose Rock Paper or Scissors");
+    let player = prompt("Choose Rock Paper or Scissors");
     // turn choice into lowercase
     playerChoice = player.toLowerCase();
-    console.log(playerChoice);
+    // To prevent another input except Rock, Paper and Scissors
     if (playerChoice == "rock" || playerChoice == "paper" || playerChoice == "scissors") {
         return playerChoice;
     }
@@ -21,12 +19,12 @@ function getPlayerChoice() {
         console.log("Please Choose Only Rock, Paper or Scissors");
         getPlayerChoice();
     }
-
 }
+
 // make function to make computer choose
 function getComputerChoice() {
     // Choosing random number from 0 to 1
-    computer = Math.random();
+    let computer = Math.random();
     // if the number less or equal to 0.33 then it will choose Rock
     if (computer <= 0.33) {
         computerChoice = "Rock";
@@ -42,7 +40,6 @@ function getComputerChoice() {
         computerChoice = "Scissors";
         return computerChoice;
     }
-
 }
 
 // make function to play RPS
@@ -51,42 +48,39 @@ function play(playerChoice, computerChoice) {
     if (playerChoice === "rock" && computerChoice === "Scissors") {
         console.log("You Win!, Rock Beats Scissors");
         pscore += 1;
-        console.log(`player = ${pscore} computer = ${cscore}`);
     }
     else if (playerChoice === "paper" && computerChoice === "Rock") {
         console.log("You Win!, Paper Beats Rock");
         pscore += 1;
-        console.log(`player = ${pscore} computer = ${cscore}`);
     }
     else if (playerChoice === "scissors" && computerChoice === "Paper") {
         console.log("You Win!, Scissors Beats Paper");
         pscore += 1;
-        console.log(`player = ${pscore} computer = ${cscore}`);
     }
     else if (playerChoice === "rock" && computerChoice === "Paper") {
         console.log("You Lose!, Paper beats Rock");
         cscore += 1;
-        console.log(`player = ${pscore} computer = ${cscore}`);
     }
     else if (playerChoice === "paper" && computerChoice === "Scissors") {
         console.log("You Lose!, Scissors beats Paper");
         cscore += 1;
-        console.log(`player = ${pscore} computer = ${cscore}`);
     }
     else if (playerChoice === "scissors" && computerChoice === "Rock") {
         console.log("You Lose!, Rock beats Scissors");
         cscore += 1;
-        console.log(`player = ${pscore} computer = ${cscore}`);
     }
     else {
         console.log("Tie Game")
-        console.log(`player = ${pscore} computer = ${cscore}`);
+
     }
+    // Show the current score
+    console.log(`player = ${pscore} computer = ${cscore}`);
 }
 
 // make function to run 5 Game RPS
 function game() {
-    for (let i = 0; i < 6; i++) {
+    // loop the program until i = 5
+    for (let i = 0; i < 5; i++) {
         // Take result of computerChoice
         computerChoice = getComputerChoice();
         // take Result of player choice
@@ -105,7 +99,6 @@ function game() {
         console.log(`Tie Game! overall score you ${pscore} computer ${cscore}`)
     }
 }
+
 // play the game
-game()
-
-
+game();
